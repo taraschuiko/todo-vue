8191ec25884c011899a056372e05c7d5dfc74c01
taraschuiko <taraschuiko@gmail.com>
2019-01-18 11:10:32 +0200

    Added todos managing and animations

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -7,27 +7,49 @@
       v-model="newTodoTitle"
       @keyup.enter="addTodo"
     >
-    <div v-for="(todo, index) in todos" :key="todo.id" class="todo-item">
-      <div class="todo-item-left">
-        <input type="checkbox" v-model="todo.completed">
-        <div
-          v-if="!todo.editing"
-          @dblclick="editTodo(todo)"
-          class="todo-item-title"
-          :class="{completed : todo.completed}"
-        >{{todo.title}}</div>
-        <input
-          v-else
-          class="todo-item-edit"
-          type="text"
-          v-model="todo.title"
-          @blur="doneEditTodo(todo)"
-          @keyup.enter="doneEditTodo(todo)"
-          @keyup.escape="cancelEditTodo(todo)"
-          v-focus
-        >
+    <transition-group
+      name="fade"
+      enter-active-class="animated fadeInUp"
+      leave-active-class="animated fadeOutDown"
+    >
+      <div v-for="(todo, index) in todosFiltered" :key="todo.id" class="todo-item">
+        <div class="todo-item-left">
+          <input type="checkbox" v-model="todo.completed">
+          <div
+            v-if="!todo.editing"
+            @dblclick="editTodo(todo)"
+            class="todo-item-title"
+            :class="{completed : todo.completed}"
+          >{{todo.title}}</div>
+          <input
+            v-else
+            class="todo-item-edit"
+            type="text"
+            v-model="todo.title"
+            @blur="doneEditTodo(todo)"
+            @keyup.enter="doneEditTodo(todo)"
+            @keyup.escape="cancelEditTodo(todo)"
+            v-focus
+          >
+        </div>
+        <div class="todo-item-remove" @click="removeTodo(index)">&times;</div>
       </div>
-      <div class="todo-item-remove" @click="removeTodo(index)">&times;</div>
+    </transition-group>
+    <div class="todo-manage">
+      <label>
+        <input type="checkbox" :checked="!anyRemaining" @change="checkAllTodos"> Check all
+      </label>
+      <div
+        class="todo-clear-completed"
+        v-if="showRemoveCompletedButton"
+        @click="removeCompletedTodos"
+      >Remove completed</div>
+      <div>{{remaining}} items left</div>
+    </div>
+    <div class="todo-filter">
+      <div :class="{active: filter == 'all'}" @click="filter = 'all'">All</div>
+      <div :class="{active: filter == 'active'}" @click="filter = 'active'">Active</div>
+      <div :class="{active: filter == 'completed'}" @click="filter = 'completed'">Completed</div>
     </div>
   </div>
 </template>
@@ -40,6 +62,7 @@ export default {
       newTodoTitle: "",
       newTodoId: 3,
       beforeEditCache: "",
+      filter: "all",
       todos: [
         {
           id: 1,
@@ -55,6 +78,27 @@ export default {
         }
       ]
     };
+  },
+  computed: {
+    remaining() {
+      return this.todos.filter(todo => !todo.completed).length;
+    },
+    anyRemaining() {
+      return this.remaining != 0;
+    },
+    todosFiltered() {
+      if (this.filter === "all") {
+        return this.todos;
+      } else if (this.filter === "active") {
+        return this.todos.filter(todo => !todo.completed);
+      } else if (this.filter === "completed") {
+        return this.todos.filter(todo => todo.completed);
+      }
+      return this.todos;
+    },
+    showRemoveCompletedButton() {
+      return this.todos.filter(todo => todo.completed).length > 0;
+    }
   },
   directives: {
     focus: {
@@ -94,12 +138,20 @@ export default {
     },
     removeTodo(index) {
       this.todos.splice(index, 1);
+    },
+    checkAllTodos() {
+      this.todos.map(todo => (todo.completed = event.target.checked));
+    },
+    removeCompletedTodos() {
+      this.todos = this.todos.filter(todo => !todo.completed);
     }
   }
 };
 </script>
 
 <style lang="scss">
+@import url("https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css");
+
 .todo-input {
   width: 100%;
   padding: 10px 18px;
@@ -121,6 +173,7 @@ export default {
   display: flex;
   justify-content: space-between;
   align-items: center;
+  animation-duration: 0.3s;
 
   &-left {
     display: flex;
@@ -155,6 +208,36 @@ export default {
     &:hover {
       color: #000;
     }
+  }
+}
+
+.todo-manage {
+  display: flex;
+  justify-content: space-between;
+  padding: 10px 19px;
+  margin-top: 10px;
+  border-top: 1px solid rgb(235, 235, 235);
+
+  .todo-clear-completed {
+    cursor: pointer;
+  }
+}
+
+.todo-filter {
+  display: flex;
+  justify-content: center;
+  margin-top: 10px;
+
+  div {
+    cursor: pointer;
+    margin: 0 10px;
+    padding: 4px 8px;
+    border-radius: 4px;
+    border: 1px solid rgb(235, 235, 235);
+  }
+
+  .active {
+    border-color: rgb(172, 172, 172);
   }
 }
 </style>
